337d9a8fd191abc1ef2b2412b0e884c2c23408e6
Sergio Valencia <sergio.valencia619@yahoo.com>
2020-11-04 15:04:48 -0700

    created first test for FizzBuzz where it simply prints all numbers between 1 and n(100)

---
+++ FizzBuzzTDD/src/main/java/FizzBuzzTesting/FizzBuzz.java
@@ -0,0 +1,9 @@
+package FizzBuzzTesting;
+
+public class FizzBuzz {
+    public static void fizzBuzz(int num){
+        for (int i = 1; i<=num; i++){
+            System.out.print(i + " ");
+        }
+    }
+}
---
+++ FizzBuzzTDD/src/test/java/FizzBuzzTesting/FizzBuzzTest.java
@@ -0,0 +1,11 @@
+package FizzBuzzTesting;
+
+import org.junit.Test;
+
+public class FizzBuzzTest {
+
+    @Test
+    public void shouldPrint1to100(){
+        FizzBuzz.fizzBuzz(100);
+    }
+}
